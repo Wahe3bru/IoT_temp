0575aff31eb87505a99b88995b87555316b8793e
Wahe3bru <wahe3bru@gmail.com>
2019-03-20 10:11:11 +0200

    successful stats extraction

--- 02-daily_mean.py
+++ 02-daily_mean.py
@@ -6,7 +6,8 @@ client = pygsheets.authorize()
 
 current_time = datetime.datetime.now()
 year = current_time.strftime("%Y")
-date = current_time.strftime("%Y-%m-%d %H:%M")
+#date = current_time.strftime("%Y-%m-%d %H:%M")
+date = "2019-03-02"
 
 # Open the spreadsheet and this months sheet .
 sh = client.open('2019-03_environmentals')
@@ -19,15 +20,16 @@ wks = sh.sheet1
 #                              'Temperature-Max', 'Humidity'])
 
 row_counts = 0
-sum_temp = 0
-sum_hum = 0
+day_temp = []
+day_hum = []
 
-# for row in wks:
-#     if row[0].str.split(' ')[0] == date:
-#         row_counts +=1
-#         sum_temp += int(row[1])
-#         sum_hum += int(row[2])
-#
-# print(sum_temp.min())
 for row in wks:
-    print(row_counts,': ',row[0])
+    date_col = row[0]
+    if date_col.split(' ')[0] == date:
+        row_counts +=1
+        day_temp.append(int(row[1]))
+        # sum_hum += int(row[2])
+
+print('min is ', np.min(day_temp))
+print('average is ', np.mean(day_temp))
+print('max is ', np.max(day_temp))
