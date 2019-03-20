6b920b55ab59cd90cb8007e1748a04e79e38181c
Wahe3bru <wahe3bru@gmail.com>
2019-03-20 09:51:45 +0200

    figuring out data to collect (filter)

--- 02-daily_mean.py
+++ 02-daily_mean.py
@@ -1,7 +1,7 @@
 import datetime
 import pygsheets
 import pandas as pd
-
+import numpy as np
 client = pygsheets.authorize()
 
 current_time = datetime.datetime.now()
@@ -9,10 +9,25 @@ year = current_time.strftime("%Y")
 date = current_time.strftime("%Y-%m-%d %H:%M")
 
 # Open the spreadsheet and this months sheet .
-sh = client.open('daily_stats')
+sh = client.open('2019-03_environmentals')
+wks = sh.sheet1
+# try:
+#     wks = sh.worksheet_by_title(year)
+# except:
+#     wks = sh.add_worksheet(year, rows=0)
+#     wks.append_table(values=['Date', 'Temperature-Min', 'Temperature-Mean',
+#                              'Temperature-Max', 'Humidity'])
 
-try:
-    wks = sh.worksheet_by_title(year)
-except:
-    wks = sh.add_worksheet(year, rows=0)
-    wks.append_table(values=['Date', 'Temperature-Min', 'Temperature-Mean', 'Temperature-Max', 'Humidity'])
+row_counts = 0
+sum_temp = 0
+sum_hum = 0
+
+# for row in wks:
+#     if row[0].str.split(' ')[0] == date:
+#         row_counts +=1
+#         sum_temp += int(row[1])
+#         sum_hum += int(row[2])
+#
+# print(sum_temp.min())
+for row in wks:
+    print(row_counts,': ',row[0])
