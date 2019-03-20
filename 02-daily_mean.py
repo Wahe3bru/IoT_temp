34573508688c1b20f25d9df7226ed7fbf40fa255
Wahe3bru <wahe3bru@gmail.com>
2019-03-20 11:16:20 +0200

    correct source gsheet and correct output gsheet

--- 02-daily_mean.py
+++ 02-daily_mean.py
@@ -1,23 +1,22 @@
 import datetime
 import pygsheets
-import pandas as pd
 import numpy as np
+
 client = pygsheets.authorize()
 
 current_time = datetime.datetime.now()
+date = current_time.strftime("%Y-%m-%d")
+month_year = current_time.strftime("%b-%Y")
 year = current_time.strftime("%Y")
-#date = current_time.strftime("%Y-%m-%d %H:%M")
-date = "2019-03-02"
 
 # Open the spreadsheet and this months sheet .
-sh = client.open('2019-03_environmentals')
-wks = sh.sheet1
-# try:
-#     wks = sh.worksheet_by_title(year)
-# except:
-#     wks = sh.add_worksheet(year, rows=0)
-#     wks.append_table(values=['Date', 'Temperature-Min', 'Temperature-Mean',
-#                              'Temperature-Max', 'Humidity'])
+sh = client.open('IoT_env')
+try:
+    wks = sh.worksheet_by_title(month_year)
+except:
+    wks = sh.add_worksheet(month_year, rows=0)
+    wks.append_table(values=['timestamp', 'temperature', 'humidity'])
+
 
 row_counts = 0
 day_temp = []
@@ -25,11 +24,33 @@ day_hum = []
 
 for row in wks:
     date_col = row[0]
-    if date_col.split(' ')[0] == date:
+    if date_col.split(' ')[0] == date: #is this best way to filter date?
         row_counts +=1
         day_temp.append(int(row[1]))
-        # sum_hum += int(row[2])
+        day_hum.append(int(row[1]))
 
-print('min is ', np.min(day_temp))
-print('average is ', np.mean(day_temp))
-print('max is ', np.max(day_temp))
+# do i create vars below, or plug them directory into day_stats_row list??
+temp_min =  str(np.min(day_temp))
+temp_mean = str(np.mean(day_temp))
+temp_max = str(np.max(day_temp))
+
+hum_min =  str(np.min(day_hum))
+hum_mean = str(np.mean(day_hum))
+hum_max = str(np.max(day_hum))
+
+day_stats_row = [date, temp_min, temp_mean, temp_max, hum_min, hum_mean, hum_max]
+
+sh_write = client.open('daily_stats')
+try:
+    wks_write = sh_write.worksheet_by_title(year)
+except:
+    wks_write = sh_write.add_worksheet(year, rows=0)
+    wks_write.append_table(values=['Date', 'Temperature-Min', 'Temperature-Mean',
+                             'Temperature-Max', 'Humidity-Min', 'Humidity-Mean',
+                             'Humidity-Max',])
+
+wks_write.append_table(values=day_stats_row)
+
+# possible enhancements
+# add summary of month, streaks (hottest, coldest, etc)
+# add weather condition sourced from weather api
