531f4d07128265a8eeb6a2bd3085f102e768f4e9
Wahe3bru <wahe3bru@gmail.com>
2019-03-22 14:10:31 +0200

    refactor

--- 03-weather_api.py
+++ 03-weather_api.py
@@ -1,46 +1,49 @@
 
 import requests
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+import helper
 import datetime
 import pygsheets
+import os
+def create_OWM_api_call(OWMapi_key, city_id='&id=3370352',
+                        city_name="&q=Cape Town", use_id=False):
+    ''' '''
+    metric_units="&units=metric"
+    BASE_URL="http://api.openweathermap.org/data/2.5/weather?appid="
+    if use_id:
+        api_call = BASE_URL + OWMapi_key + city_id + metric_units
+    else:
+        api_call = BASE_URL + OWMapi_key + city_name + metric_units
+    return api_call
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+def get_weather_from_OWM(api_call):
+    weather_data = requests.get(api_call).json()
+    temperature = weather_data['main']['temp']
+    humidity = weather_data['main']["humidity"]
+    pressure  = weather_data['main']['pressure']
+    description = weather_data['weather'][0]['description']
+    return [humidity, temperature, pressure, description]
 
-# Accessing variables.
-api_key = os.getenv('OWMapi_key')
+def main():
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M")
+    month_year = current_time.strftime("%b-%Y")
 
-OWMapi_key = api_key
-cape_town_id = '&id=3370352'
-city_name = "&q=Cape Town"
-metric_units = "&units=metric"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid="
-api_call = BASE_URL + OWMapi_key + city_name + metric_units
+    client = pygsheets.authorize()
+    sh = client.open('Outside_env')
+    try:
+        wks = sh.worksheet_by_title(month_year)
+    except:
+        wks = sh.add_worksheet(month_year, rows=0)
+        wks.append_table(values=['timestamp', 'temperature', 'humidity', 'pressure', 'description'])
 
-weather_data = requests.get(api_call).json()
-temperature = weather_data['main']['temp']
-humidity = weather_data['main']["humidity"]
-pressure  = weather_data['main']['pressure']
-description = weather_data['weather'][0]['description']
+    api_key = os.getenv('OWMapi_key')
+    api_call = create_OWM_api_call(api_key)
+    outside_env = get_weather_from_OWM(api_call)
 
-current_time = datetime.datetime.now()
-timestamp = current_time.strftime("%Y-%m-%d %H:%M")
-month_year = current_time.strftime("%b-%Y")
+    row = [timestamp] + outside_env
 
-client = pygsheets.authorize()
-# Open the spreadsheet and this months sheet .
-sh = client.open('Outside_env')
+    # append row
+    wks.append_table(values=row)
 
-try:
-    wks = sh.worksheet_by_title(month_year)
-except:
-    wks = sh.add_worksheet(month_year, rows=0)
-    wks.append_table(values=['timestamp', 'temperature', 'humidity', 'description'])
-
-
-row = [timestamp, temperature, humidity, description]
-
-# append row
-wks.append_table(values=row)
+if __name__ == '__main__':
+    main()
