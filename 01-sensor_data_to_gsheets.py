f00d546f10c558387ae4dc1b44394dcafdb7a16e
Wahe3bru <wahe3bru@gmail.com>
2019-03-22 14:51:42 +0200

    refactor using helper.open_worksheet()

--- 01-sensor_data_to_gsheets.py
+++ 01-sensor_data_to_gsheets.py
@@ -1,27 +1,21 @@
 #! /usr/bin/python3
 
 import datetime
-import pygsheets
 import Adafruit_DHT
+import helper
 
-client = pygsheets.authorize()
+def main():
+    current_time = datetime.datetime.now()
+    month_year = current_time.strftime("%b-%Y")
 
-current_time = datetime.datetime.now()
-month_year = current_time.strftime("%b-%Y")
-month_year
+    timestamp = current_time.strftime("%Y-%m-%d %H:%M")
+    humidity, temperature = Adafruit_DHT.read_retry(11, 17)
+    row = [timestamp, temperature, humidity]
 
-# Open the spreadsheet and this months sheet .
-sh = client.open('IoT_env')
+    columns = ['timestamp', 'temperature', 'humidity']
+    wks = helper.open_worksheet('IoT_env', month_year, columns)
+    # append row
+    wks.append_table(values=row)
 
-try:
-    wks = sh.worksheet_by_title(month_year)
-except:
-    wks = sh.add_worksheet(month_year, rows=0)
-    wks.append_table(values=['timestamp', 'temperature', 'humidity'])
-
-timestamp = current_time.strftime("%Y-%m-%d %H:%M")
-humidity, temperature = Adafruit_DHT.read_retry(11, 17)
-row = [timestamp, temperature, humidity]
-
-# append row
-wks.append_table(values=row)
+if __name__ == '__main__':
+    main()
--- 02-daily_stats.py
+++ 02-daily_stats.py
@@ -1,60 +1,49 @@
 #! /usr/bin/python3
 
 import datetime
-import pygsheets
+import helper
 import numpy as np
 
-client = pygsheets.authorize()
+def main():
+    current_time = datetime.datetime.now()
+    date = current_time.strftime("%Y-%m-%d")
+    month_year = current_time.strftime("%b-%Y")
+    year = current_time.strftime("%Y")
 
-current_time = datetime.datetime.now()
-date = current_time.strftime("%Y-%m-%d")
-month_year = current_time.strftime("%b-%Y")
-year = current_time.strftime("%Y")
+    # Open the spreadsheet and this months sheet .
+    wks = helper.open_worksheet('IoT_env', month_year, columns)
 
-# Open the spreadsheet and this months sheet .
-sh = client.open('IoT_env')
-try:
-    wks = sh.worksheet_by_title(month_year)
-except:
-    wks = sh.add_worksheet(month_year, rows=0)
-    wks.append_table(values=['timestamp', 'temperature', 'humidity'])
+    row_counts = 0
+    day_temp = []
+    day_hum = []
 
+    for row in wks:
+        date_col = row[0]
+        if date_col.split(' ')[0] == date: #is this best way to filter date?
+            row_counts +=1
+            day_temp.append(int(row[1]))
+            day_hum.append(int(row[1]))
 
-row_counts = 0
-day_temp = []
-day_hum = []
+    # do i create vars below, or plug them directory into day_stats_row list??
+    temp_min =  str(np.min(day_temp))
+    temp_mean = str(np.mean(day_temp))
+    temp_max = str(np.max(day_temp))
 
-for row in wks:
-    date_col = row[0]
-    if date_col.split(' ')[0] == date: #is this best way to filter date?
-        row_counts +=1
-        day_temp.append(int(row[1]))
-        day_hum.append(int(row[1]))
+    hum_min =  str(np.min(day_hum))
+    hum_mean = str(np.mean(day_hum))
+    hum_max = str(np.max(day_hum))
+    log_counts = str(row_counts)
 
-# do i create vars below, or plug them directory into day_stats_row list??
-temp_min =  str(np.min(day_temp))
-temp_mean = str(np.mean(day_temp))
-temp_max = str(np.max(day_temp))
+    day_stats_row = [date, temp_min, temp_mean, temp_max, hum_min, hum_mean,
+                     hum_max, log_counts]
 
-hum_min =  str(np.min(day_hum))
-hum_mean = str(np.mean(day_hum))
-hum_max = str(np.max(day_hum))
-log_counts = str(row_counts)
+    columns=['Date', 'Temperature-Min', 'Temperature-Mean','Temperature-Max',
+             'Humidity-Min', 'Humidity-Mean', 'Humidity-Max', '#Records']
+    wks_write = helper.open_worksheet('daily_stats', year, columns)
+    wks_write.append_table(values=day_stats_row)
 
-day_stats_row = [date, temp_min, temp_mean, temp_max, hum_min, hum_mean,
-                 hum_max, log_counts]
-
-sh_write = client.open('daily_stats')
-try:
-    wks_write = sh_write.worksheet_by_title(year)
-except:
-    wks_write = sh_write.add_worksheet(year, rows=0)
-    wks_write.append_table(values=['Date', 'Temperature-Min', 'Temperature-Mean',
-                             'Temperature-Max', 'Humidity-Min', 'Humidity-Mean',
-                             'Humidity-Max', '#Records'])
-
-wks_write.append_table(values=day_stats_row)
-
+if __name__ == '__main__':
+    main()
 # possible enhancements
 # add summary of month, streaks (hottest, coldest, etc)
 # add weather condition sourced from weather api
