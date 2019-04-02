98b9f8242cc3a878c13f406513bb1a27397d07fd
Wahe3bru <wahe3bru@gmail.com>
2019-04-02 15:54:11 +0200

    get list of worksheet titles.

--- 04-getting_all_wks.py
+++ 04-getting_all_wks.py
@@ -1,8 +1,21 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
+import pandas as pd
 import pygsheets
 
 def main():
+    #from helper_dash.py
+    def open_worksheet(spreadsheet_name, worksheet_title):
+        client = pygsheets.authorize()
+        sh = client.open(spreadsheet_name)
+        wks = sh.worksheet_by_title(worksheet_title)
+        return wks
+
+    def worksheet_as_df(spreadsheet_name, worksheet_title):
+        wks = open_worksheet(spreadsheet_name, worksheet_title)
+        df = wks.get_as_df()
+        return df
+
     spreadsheet_name ='IoT_env'
 
     client = pygsheets.authorize()
@@ -10,11 +23,15 @@ def main():
     def get_worksheet_titles(spreadsheet_name):
         sh = client.open(spreadsheet_name)
         worksheet_titles = sh.worksheets()
-        wks_titles = []
-        for title in worksheet_titles:
-            wks_titles.append(str(title).split("'")[1])
-        return wks_titles
+        return [str(title).split("'")[1] for title in worksheet_titles]
 
-    print(get_worksheet_titles(spreadsheet_name))
+    worksheets = get_worksheet_titles(spreadsheet_name)
+
+    all_df = []
+    for title in worksheets:
+         df = worksheet_as_df(spreadsheet_name,title)
+         print(title, len(df))
+
+    # need to create one big df
 if __name__ == '__main__':
     main()
